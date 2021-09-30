770b799f00c2d908c31a3dfe825197c4c044957b
ErickOS-Redes <61870100+ErickOS-Redes@users.noreply.github.com>
2021-09-30 14:15:48 -0300

    Repositories finalizado (falta imagem, mas blz)

---
+++ senai_spmedicalgroup_webAPI/senai_spmedicalgroup_webAPI/Repositories/UsuarioRepository.cs
@@ -0,0 +1,91 @@
+﻿using senai_spmedicalgroup_webAPI.Contexts;
+using senai_spmedicalgroup_webAPI.Domains;
+using senai_spmedicalgroup_webAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_spmedicalgroup_webAPI.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        readonly SPMEDICALGROUPContext context = new();
+        public void AtualizarURL(int idUsuario, Usuario usuarioAtualizado)
+        {
+            Usuario usuarioBuscado = context.Usuarios.Find(idUsuario);
+
+            if (usuarioAtualizado.Email != null || usuarioAtualizado.Senha != null)
+            {
+                usuarioBuscado.Email = usuarioAtualizado.Email;
+                usuarioBuscado.Senha = usuarioAtualizado.Senha;
+
+                context.Usuarios.Update(usuarioBuscado);
+
+                context.SaveChanges();
+            }
+        }
+
+        public Usuario BuscarPorID(int idUsuario)
+        {
+            return context.Usuarios
+                .Select(u => new Usuario
+                {
+                    IdTipoUsuarioNavigation = new Tipousuario
+                    {
+                        DescricaoTipoUsuario = u.IdTipoUsuarioNavigation.DescricaoTipoUsuario,
+                    },
+                    IdMedicoNavigation = new Medico
+                    {
+                        NomeMedico = u.IdMedicoNavigation.NomeMedico,
+                        SobrenomeMedico = u.IdMedicoNavigation.SobrenomeMedico,
+                    },
+                    IdPacienteNavigation = new Paciente
+                    {
+                        NomePaciente = u.IdPacienteNavigation.NomePaciente,
+                    },
+                    Email = u.Email,
+                })
+                .FirstOrDefault(u => u.IdUsuario == idUsuario);
+                
+        }
+
+        public void Cadastrar(Usuario novoUsuario)
+        {
+            context.Usuarios.Add(novoUsuario);
+
+            context.SaveChanges();
+
+        }
+
+        public List<Usuario> ListarTodos(int idUsuario)
+        {
+            return context.Usuarios
+                .Select(u => new Usuario
+                {
+                    IdTipoUsuarioNavigation = new Tipousuario
+                    {
+                        DescricaoTipoUsuario = u.IdTipoUsuarioNavigation.DescricaoTipoUsuario,
+                    },
+                    IdMedicoNavigation = new Medico
+                    {
+                        NomeMedico = u.IdMedicoNavigation.NomeMedico,
+                        SobrenomeMedico = u.IdMedicoNavigation.SobrenomeMedico,
+                    },
+                    IdPacienteNavigation = new Paciente
+                    {
+                        NomePaciente = u.IdPacienteNavigation.NomePaciente,
+                    },
+                    Email = u.Email,
+                })
+                .ToList();
+        }
+
+        public void Remover(int idUsuario)
+        {
+            context.Usuarios.Remove(BuscarPorID(idUsuario));
+
+            context.SaveChanges();
+        }
+    }
+}
